af3b65da0b78fca615c0b2ea1b699957c86e61f2
Mary Parker <98114662+m6parker@users.noreply.github.com>
2022-12-06 14:15:22 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,3 +1,6 @@
+// Mary Parker
+// Prisca Jean-Pierre
+
 /*
 main(){
 
@@ -24,28 +27,28 @@ main() {
 		syscall(0, "SHELL>> ");
 		syscall(1, line);
 
-		if (line[0] == 't' &&
+		if(line[0] == 't' &&
 			line[1] == 'y' &&
 			line[2] == 'p' &&
 			line[3] == 'e'  &&
 			line[4] == ' ') {
 			syscall(3, line+5, buffer, &sectorsRead);
 			if (sectorsRead > 0)
-				syscall(0, buffer);
+			    syscall(0, buffer);
 			else
-				syscall(0, "the file does not exist.\r\n");
-		}
-		else if (line[0] == 'e' &&
-				line[1] == 'x' &&
-				line[2] == 'e' &&
-				line[3] == 'c'  &&
-				line[4] == ' ') {
-				syscall(3, line+5, buffer, &sectorsRead);
-				if (sectorsRead > 0)
-					syscall(4, line+5);
-				else
-					syscall(0, "unknown program\r\n");
-		}
+			    syscall(0, "the file does not exist.\r\n");
+		}//if type
+		else if(line[0] == 'e' &&
+			line[1] == 'x' &&
+			line[2] == 'e' &&
+			line[3] == 'c'  &&
+			line[4] == ' ') {
+			syscall(3, line+5, buffer, &sectorsRead);
+			if (sectorsRead > 0)
+				syscall(4, line+5);
+			else
+				syscall(0, "unknown program\r\n");
+		}//if exec
 		else
 			syscall(0, "unknown command\r\n");
 	}
